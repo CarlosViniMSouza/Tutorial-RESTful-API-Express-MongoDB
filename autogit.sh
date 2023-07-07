cf81d75b1eb23198a580e3f8f4f617a67f268262
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2023-07-07 11:24:50 -0300

    stopped in: MongoDB setup

--- autogit.sh
+++ autogit.sh
@@ -3,4 +3,4 @@ git commit -m "stopped in: MongoDB setup"
 git push -u origin main
 git status
 
-# continue in: https://youtu.be/H9M02of22z4?t=2060
+# continue in: https://youtu.be/H9M02of22z4?t=2255
