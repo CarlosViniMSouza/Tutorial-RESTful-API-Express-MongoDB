f201b40e328b714ea56146fbf2152ba91a76f27a
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2023-07-07 17:40:25 -0300

    stopped in: CRUD getNews

---
+++ models/newsModel.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const newsSchema = mongoose.Schema({
+    title: {
+        type: 'string',
+        required: [true, "Add the Title!"]
+    },
+    subtitle: {
+        type: 'string',
+        required: [true, "Add the new Subtitle!"],
+    },
+    paragraph1: {
+        type: 'string',
+        required: [true, "Add the new Paragraph 1!"],
+    },
+    paragraph2: {
+        type: 'string',
+        required: [true, "Add the new Paragraph 2!"],
+    },
+    paragraph3: {
+        type: 'string',
+        required: [true, "Add the new Paragraph 3!"],
+    }
+});
+
+module.exports = mongoose.model("News", newsSchema);
